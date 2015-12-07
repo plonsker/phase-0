b4cdb09114aa0421b9e2098090a1ab19b5fa17d8
plonsker <joeplonsker@gmail.com>
2015-12-06 19:23:09 -0800

    Updating tests

--- week-7/group-project/tests.js
+++ week-7/group-project/tests.js
@@ -1,12 +1,44 @@
 
 // Add the finished solution here when you receive it.
 // __________________________________________
+function arrayMath(array){
+
+  var sum = getSum(array), median = getMedian(array), mean = getMean(array);
+  console.log('Your calculations are complete, the sum is '+sum+', the median is '+median+' and the mean is '+mean);
+};
+
+
+//As a user, I want to be able to submit a group of numbers then it will return the sum of all the numbers in the group.
+function getSum(array){
+  return array.reduce(function(a,b) {return a+b;});  //Not supported by all browsers
+};
+
+//As a user, I want to be able to submit a group of numbers then it will return the median of the group.
+function getMedian(array){
+  array.sort(function(a, b){return a-b});
+  var len = array.length;
+  if(len % 2 == 0){
+    return (array[len/2] + array[(len/2)-1])/2;
+  }
+  else{
+    return array[(len-1)/2];
+  }
+};
+
+//As a user, I want to be able to submit a group of numbers then it will return the average of the group.
+function getMean(array){
+  return getSum(array)/array.length;
+};
+
+arrayMath([5, 1, 9, 10, 100]);
+
 // Tests:  Do not alter code below this line.
 
 //As a user, I need a program that calculates the sum, mean, and median of an array, regardless of
 //length.  
 
-
+oddLengthArray  = [1, 2, 3, 4, 5, 5, 7]
+evenLengthArray = [4, 4, 5, 5, 6, 6, 6, 7]
 
 
 function assert(test, message, test_number) {
